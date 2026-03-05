74afd7314eccbce2722ae51d966efd0f5e338dd3
Vincent Dorange <aiconsulting06000@gmail.com>
2026-03-05 23:05:57 +0100

    ACF Control v3.0

---
+++ app/layout.tsx
@@ -0,0 +1,5 @@
+// @ts-nocheck 
+export const metadata = { title: "ACF Control" }; 
+export default function RootLayout({ children }: { children: React.ReactNode }) { 
+  return <html lang="en"><body style={{ margin: 0 }}>{children}</body></html>; 
+} 
